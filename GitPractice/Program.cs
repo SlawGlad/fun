006e2cc2219fe5535c7bd76fb4e5f89ddd99145e
SlawGlad <sla.gladys@interia.pl>
2019-01-05 15:20:43 +0100

    Zmiany

--- GitPractice/Program.cs
+++ GitPractice/Program.cs
@@ -10,7 +10,7 @@ namespace GitPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("HelloBaza");
         }
     }
 }
